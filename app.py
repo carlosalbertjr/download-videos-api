c122e27c94fa69571ed30aedc6807de984136a09
Carlos Junior <carlosalberto.jr049@gmail.com>
2025-05-20 15:44:40 -0300

    update api geo

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import os
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import yt_dlp
 
 app = Flask(__name__)
+CORS(app, resources={r"/download": {"origins": "http://localhost:5000"}})   # <- Isso permite requisições de qualquer origem
 
 # Carregar variáveis do .env
 PORT = int(os.getenv("PORT", 5000))  # Se não achar no .env, usa 5000
@@ -16,8 +18,14 @@ if not os.path.exists(DOWNLOAD_PATH):
 def home():
     return jsonify({"message": "Servidor rodando! Envie um POST para /download"}), 200
 
-@app.route("/download", methods=["POST"])
+@app.route("/download", methods=['POST', 'OPTIONS'])
 def download_video():
+    if request.method == 'OPTIONS':
+        # Resposta ao preflight
+        response = app.make_default_options_response()
+        response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
+        return response
+
     data = request.json
     urls = data.get("urls", [])
 
